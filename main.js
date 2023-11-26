9aa319e24e2efee02a69af04ea60df63a0b3e3b5
Diatom <Supriore@gmail.com>
2023-11-26 21:11:13 +0300

    update

--- main.js
+++ main.js
@@ -1,67 +1,66 @@
-const fet = await fetch (`./main/about.md`);
-console.log(fet);
-
-const body = await fet.text();
-console.log(body);
+const fet = await fetch (`./main/about.md`)
+const body = await fet.text()
+console.log(body)
 
 class Templ {
-  constructor(head, main) {
-    this.head = head;
-    this.main = main;
+  constructor(head, main, footer) {
+    this.head = head
+    this.main = main
+    this.footer = footer
     }
   displayHead() {
-    document.getElementById(`bodymain`).appendChild(this.head);
+    document.getElementById(`bodymain`).appendChild(this.head)
   }
   displayMain() {
-    document.getElementById(`bodymain`).appendChild(this.main);
+    document.getElementById(`bodymain`).appendChild(this.main)
   }
 }
 class Head {
   constructor(nav, header) {
-    this.nav = nav;
-    this.header = header;
+    this.nav = nav
+    this.header = header
   }
   displayNav() {
-    const header = document.createElement(`header`);
-    const nav = document.createElement(`nav`);
+    const header = document.createElement(`header`)
+    const nav = document.createElement(`nav`)
     for (const val of this.nav) {
-      const item = document.createElement(`a`);
-      item.innerHTML = val;
+      const item = document.createElement(`a`)
+      item.innerHTML = val
     }
   }
 }
 class Main {
   constructor() {
-    this.about = about;
-    this.latestarticle = latestarticle;
-    this.skills = skills;
+    this.about = about
+    this.latestarticle = latestarticle
+    this.skills = skills
   }
   displayAbout() {
-    innerHTML = document.createElement(`div`);
+    innerHTML = document.createElement(`div`)
   }
   displayLatestarticle() {
-    const divLast = document.createElement(`div`);
+    const divLast = document.createElement(`div`)
     for (let i = 0; i < 3; i++) {
-      const latestUp = article.md;
-      latestUp.innerHTML = this.latestarticle;
-      divLast.appendChild(latestUp);
+      const latestUp = article.md
+      latestUp.innerHTML = this.latestarticle
+      divLast.appendChild(latestUp)
     }
   }
   displaySkills() {
-    const mySkills = document.createElement(`div`);
-    innerHTML.appendChild(mySkills);
+    const mySkills = document.createElement(`div`)
+    innerHTML.appendChild(mySkills)
     mySkills.appendChild(this.skills)
   }
 }
-let head = new Head();
-let main = new Main();
-let templ = new Templ(main, head);
-templ.displayHead;
-templ.displayMain;
+let head = new Head()
+let main = new Main()
+let templ = new Templ(main, head)
+templ.displayHead
+templ.displayMain
 
 class TemplMain {
   constructor() {
-    this.main = document.createElement(`main`);
+    this.main = document.createElement(`main`)
   }
   displayHead() {
     
