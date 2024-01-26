19eb2d2fcb0a6dfda5396200a46f361c6b5dad1e
Diatom <Supriore@gmail.com>
2024-01-26 13:29:26 +0300

    update

--- blog.js
+++ blog.js
@@ -3,9 +3,22 @@ import { marked } from './marked-lib.js'
 import { menu, contact, list } from './data/data.js'
 import { templ, header, footer } from './templ.js'
 
-const fetBlog = await fetch (`./articles/${aaaaaaaaa}.md`)
-const blog = await fetBlog.text()
+const fetBlog = await fetch (`./articles/Gosudarstvo-kak-brend.md`)
+const art = await fetBlog.text()
 
+
+class Blog extends HTMLElement {
+  render() {
+      const myblog = document.createElement(`div`)
+      myblog.innerHTML = marked.parse(art)
+      this.appendChild(myblog)
+  }
+  connectedCallback() {
+      this.render()
+  }
+}
+customElements.define('my-blog', Blog)
+const blog = new Blog()
 
 class Main {
     constructor() {
