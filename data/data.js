406d0cec8dd1cda62b0ace8a1be13d370f7c3280
Diatom <Supriore@gmail.com>
2024-03-05 15:13:20 +0300

    update

--- data/data.js
+++ data/data.js
@@ -16,9 +16,6 @@ export const contact = [
         'Discord': `https://discord.com/users/214732781945618442`
     },
     {
-        'HeadHunter': `https://hh.ru/resume/796dbae3ff0577aafe0039ed1f535a4d737134`
-    },
-    {
         'GitHub': `https://github.com/Diatom/`
     }
 ]
