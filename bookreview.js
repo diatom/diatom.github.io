c4730a400fa06d1a9db745299f162615acd3b81f
Diatom <Supriore@gmail.com>
2024-03-06 17:56:27 +0300

    update

--- bookreview.js
+++ bookreview.js
@@ -6,30 +6,64 @@ import { dat } from './book-review/data-books.js'
 
 
 // Book cards
-class createList {
-  constructor(obj) {
-    this.divE = document.createElement('div')
-    this.obj = obj
-  }
-  createDivElement() {
-    for (let [key, value] of Object.entries(this.obj)) {
-      const pE = document.createElement('p')
-      pE.innerText = `${key}: ${value}`
-      this.divE.appendChild(pE)
-    }
-    return this.divE
-  }
-}
+// class createList {
+//   constructor(obj) {
+//     this.div = document.createElement('div')
+//     this.obj = obj
+//   }
+//   createDivElement() {
+//     for (let [key, value] of Object.entries(this.obj)) {
+//       const p = document.createElement('p')
+//       const span = document.createElement('span')
+//       const h3 = document.createElement('h3')
+//       p.innerText = `${key}: ${value}`
+//       this.div.appendChild(p)
+//     }
+//     return this.div
+//   }
+// }
+// class createList {
+//   constructor(obj) {
+//     this.div = document.createElement('div')
+//     this.obj = obj
+//   }
+//   createDivElement() {
+//     for (let val of this.obj) {
+//       const p = document.createElement('p')
+//       const span = document.createElement('span')
+//       const h3 = document.createElement('h3')
+//       span.innerText = val.Id
+//       h3.innerText = val.name
+//       p.innerText = val.Автор
+//       // p.innerText = val.Жанр
+//       // p.innerText = val.Дата
+//       // p.innerText = val.Описание
+//       // p.innerText = val.Рейтинг
+//       // p.innerText = val.Теги
+//       this.div.appendChild(span)
+//       this.div.appendChild(h3)
+//       this.div.appendChild(p)
+//     }
+//     return this.div
+//   }
+// }
 
 class BookList extends HTMLElement {
   render() {
-    for (const value of dat) {
-      console.log(value)
-      const myList = new createList(value)
-      const divE = myList.createDivElement()
-      divE.setAttribute(`class`, `book`)
-      divE.setAttribute(`data-index`, `${value.tags}`)
-      this.appendChild(divE)
+    for (const val of dat) {
+      const div = document.createElement('div')
+      const p = document.createElement('p')
+      const span = document.createElement('span')
+      const h3 = document.createElement('h3')
+      span.innerHTML = val.Id
+      h3.innerHTML = val.name
+      p.innerHTML = val.description
+      div.appendChild(span)
+      div.appendChild(h3)
+      div.appendChild(p)
+      div.setAttribute(`class`, `book`)
+      div.setAttribute(`data-index`, `${val.tags}`)
+      this.appendChild(div)
     }
   }
   connectedCallback() {
@@ -38,6 +72,25 @@ class BookList extends HTMLElement {
 }
 customElements.define('book-list', BookList)
 const myList = new BookList()
+
+
+// class BookList extends HTMLElement {
+//   render() {
+//     for (const val of dat) {
+//       // console.log(value)
+//       // const myList = new createList(value)
+//       const div = myList.createDivElement()
+//       div.setAttribute(`class`, `book`)
+//       div.setAttribute(`data-index`, `${val.tags}`)
+//       this.appendChild(div)
+//     }
+//   }
+//   connectedCallback() {
+//     this.render()
+//   }
+// }
+// customElements.define('book-list', BookList)
+// const myList = new BookList()
 
 
 // Main
